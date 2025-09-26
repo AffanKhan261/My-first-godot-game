e16db34e70ac811da80b2d60e3d49137f9349328
Affan Khan <khanaffan1006@gmail.com>
2025-09-26 16:42:23 +1000

    added a settings screen

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,13 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+
+@onready var settings: Panel = $Settings
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	main_buttons.visible = true
+	settings.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,9 +19,18 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
-func _on_settings_2_pressed() -> void:
+func _on_settings_2_pressed() -> void: # This is actuall for the how to play section
 	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	main_buttons.visible = false
+	settings.visible = true
+
+
+func _on_back_settings_pressed() -> void:
+	_ready()
